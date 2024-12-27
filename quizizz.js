66502606ca404930d9eed6c54bb5fd29f93b2435
Award <104771435+Awardxz@users.noreply.github.com>
2024-12-27 10:18:25 +0100

    fixed with quizzizSoft

--- quizizz.js
+++ quizizz.js
@@ -2,30 +2,33 @@
 let answers1 = [];
 
 async function fetchAnswers() {
-    const code12 = document.querySelector('.room-code.header-item-container-sizing.animated.fadeIn.anim-300-duration').textContent
-    const response = await fetch(`https://api.quizit.online/quizizz/answers?pin=${code12}`);
+
+    const code12 = document.querySelector('button.box-border:nth-child(1)').textContent
+    const code = code12.replace(/\s+/g, '');
+    const response = await fetch(`http://localhost:3000/api/code?code=${code}`,
+        {
+            redirect: 'follow',
+            method : "POST",
+        }
+    );
     const data = await response.json();
 
-    if (data.message === "Ok" && data.data && data.data.answers) {
-        answers1 = data.data.answers.map(answer => {
-            const questionText = answer.question.text.replace(/<[^>]*>?/gm, '').trim();
-            const answerText = answer.answers[0].text.replace(/<[^>]*>?/gm, '').trim(); 
-            return { question: questionText, answer: answerText };
-        });
-    } else {
-        throw new Error("Error fetching answers: " + data.message);
-    }
+    answers1 = data.answers;
+    return data.answers;
 }
 
 
 function checkForQuestion() {
     const questionElement = document.querySelector('.resizeable.gap-x-2.question-text-color.text-light');
+    const answerElement = document.createElement('p')
     if (questionElement) {
         const currentQuestion = questionElement.textContent.trim();
         const matchingPair = answers1.find(pair => pair.question === currentQuestion);
         if (matchingPair) {
             console.log("Found question:", matchingPair.question);
-            console.log("Answer:", matchingPair.answer);
+            console.log("Answer:", matchingPair.answers);
+            answerElement.textContent = matchingPair.answers;
+            questionElement.append(answerElement)
         } else {
             console.log("Question found, but no matching answer in the array.");
         }
@@ -40,4 +43,3 @@ function checkForQuestion() {
 fetchAnswers()
     .then(checkForQuestion)
     .catch(error => console.error(error));
-
